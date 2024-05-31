626264e6ecfc8f02c0d021ed5cf5ecb13ecd5a8f
frostyfan109 <groupe@renci.org>
2024-05-31 15:30:42 -0400

    Add SSH private URL gitea auth

--- eduhelx_jupyterlab_student/handlers.py
+++ eduhelx_jupyterlab_student/handlers.py
@@ -242,24 +242,41 @@ async def create_repo_root_if_not_exists(context: AppContext, course) -> None:
         repo_root.mkdir(parents=True)
 
 async def create_ssh_config_if_not_exists(context: AppContext, course, student) -> None:
+    settings = await context.api.get_settings()
     repo_root = context._compute_repo_root(course["name"]).resolve()
     ssh_config_dir = repo_root / ".ssh"
     ssh_config_file = ssh_config_dir / "config"
-    ssh_identity_file = ssh_config_dir / "id_rsa"
-    ssh_public_key_file = ssh_config_dir / "id_rsa.pub"
-    ssh_host = urlparse("ssh://" + student["fork_remote_url"]).hostname
-    ssh_port = 22 if not context.config.LOCAL else 2222
-    ssh_user = "git"
+    ssh_identity_file = ssh_config_dir / "id_gitea"
+    ssh_public_key_file = ssh_config_dir / "id_gitea.pub"
+
+    ssh_public_url = student["fork_remote_url"]
+    if not urlparse(ssh_public_url).scheme:
+        ssh_public_url = "ssh://" + ssh_public_url
+
+    ssh_private_url = settings["gitea_ssh_url"] if not context.config.LOCAL else "ssh://git@localhost:2222"
+    if not urlparse(ssh_private_url).scheme:
+        ssh_private_url = "ssh://" + ssh_private_url 
+
+    ssh_public_url_parsed = urlparse(ssh_public_url)
+    ssh_private_url_parsed = urlparse(ssh_private_url)
+
+    ssh_public_hostname = ssh_public_url_parsed.hostname
+    ssh_private_hostname = ssh_private_url_parsed.hostname
+    ssh_port = ssh_private_url_parsed.port or 2222
+    ssh_user = ssh_private_url_parsed.username or "git"
+    
     if not ssh_identity_file.exists():
         ssh_config_dir.mkdir(parents=True, exist_ok=True)
         execute(["ssh-keygen", "-t", "rsa", "-f", ssh_identity_file, "-N", ""])
         with open(ssh_config_file, "w+") as f:
-            f.write(
-                f"Host { ssh_host }\n" \
+            # Host (public Gitea URL) is rewritten as an alias to HostName (private ssh URL)
+            f.write( 
+                # Note that Host is really a hostname in SSH config. and is an alias to HostName here.
+                f"Host { ssh_public_hostname }\n" \
                 f"   User { ssh_user }\n" \
                 f"   Port { ssh_port }\n" \
                 f"   IdentityFile { ssh_identity_file }\n" \
-                f"   HostName localhost\n" if context.config.LOCAL else ""
+                f"   HostName { ssh_private_hostname }\n"
             )
         with open(ssh_public_key_file, "r") as f:
             public_key = f.read()
@@ -280,15 +297,26 @@ async def clone_repo_if_not_exists(context: AppContext, course, student) -> None
         master_repository_url = course["master_remote_url"]
         student_repository_url = student["fork_remote_url"]
 
+        def is_repo_populated():
+            if not repo_root.exists(): return False
+            # Literally, are there any files in the repo that aren't dot-files or in dot-directories?
+            # There will always be some files in the repo root, even if just created, such as git and ssh config
+            return any([
+                path for path in repo_root.rglob("*") if not any([
+                    part.startswith(".") for part in path.parts
+                ])
+            ])
         # We absolutely don't want to allow overriding any existing files.
         # To be extra careful, we will move the existing directory if it isn't empty.
-        if repo_root.exists() and list(repo_root.iterdir()) != [repo_root / ".git"]:
+        if is_repo_populated():
+            c = 0
             uniq_rname = str(repo_root) + "~{}"
             while os.path.exists(uniq_rname.format(c)):
                 c += 1
             repo_root.rename(uniq_rname.format(c))
             # Recreate the directory
             repo_root.mkdir()
+            print(123498102401892384, uniq_rname.format(c))
             await set_git_authentication(context, course, student)
 
         """ Now we're working with a new, empty directory. """
@@ -323,7 +351,7 @@ async def set_git_authentication(context: AppContext, course, student) -> None:
     repo_root = context._compute_repo_root(course["name"]).resolve()
     student_repository_url = student["fork_remote_url"]
     ssh_config_file = repo_root / ".ssh" / "config"
-    ssh_identity_file = repo_root / ".ssh" / "id_rsa"
+    ssh_identity_file = repo_root / ".ssh" / "id_gitea"
 
     try:
         get_git_repo_root(path=repo_root)
